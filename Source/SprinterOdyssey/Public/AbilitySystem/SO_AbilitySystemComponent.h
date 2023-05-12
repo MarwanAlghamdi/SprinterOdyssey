d9087a72c73c7331dcf484fe7d4284c17d2c685c
Theyqq <skill.gh@gmail.com>
2023-05-13 00:43:46 +0300

    prepare to build from scratch

---
+++ Source/SprinterOdyssey/Public/AbilitySystem/SO_AbilitySystemComponent.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AbilitySystemComponent.h"
+#include "SO_AbilitySystemComponent.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SPRINTERODYSSEY_API USO_AbilitySystemComponent : public UAbilitySystemComponent
+{
+	GENERATED_BODY()
+	
+};
---
+++ Source/SprinterOdyssey/Public/Player/SO_PlayerController.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "SO_PlayerController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SPRINTERODYSSEY_API ASO_PlayerController : public APlayerController
+{
+	GENERATED_BODY()
+	
+};
